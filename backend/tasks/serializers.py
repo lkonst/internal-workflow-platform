2cd2fb6a4d9737edc8e0951717f0da910acbf650
lkonst <fisikosnomos@gmail.com>
2026-03-18 13:18:22 +0200

    Delete commented code

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Task, TaskStatus
 
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
@@ -9,16 +10,7 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def validate_status(self, value):
         instance = getattr(self, "instance", None)
-        
+
         if instance is not None and not instance.can_transition_to(value):
-            raise serializers.ValidationError(
-                f"Invalid status transition from {instance.status} to {value}."
-            )
+            raise serializers.ValidationError(f"Invalid status transition from {instance.status} to {value}.")
         return value
-    
-    #  def validate_status(self, value):
-    #     if self.instance and not self.instance.can_transition_to(value):
-    #         raise serializers.ValidationError(
-    #             f"Invalid status transition from {self.instance.status} to {value}."
-    #         )
-    #     return value
